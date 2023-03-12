252e82ace37ea248b8357a66d845f78c45587909
Marton Dobos <dobeis2@gmail.com>
2023-03-12 21:16:07 +0100

    feat: util hooks

---
+++ frontend/hooks/usePager.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export type PagerProps = {
+  currentPage: number;
+  onNextPage: () => void;
+  onPrevPage: () => void;
+};
+
+type UsePagerHook = () => PagerProps;
+
+export const usePager: UsePagerHook = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onNextPage = () => setCurrentPage((page) => page + 1);
+
+  const onPrevPage = () => setCurrentPage((page) => (page > 1 ? page - 1 : page));
+
+  return {
+    currentPage,
+    onNextPage,
+    onPrevPage,
+  };
+};
